43197bda7dfe8e02206349f91363733b45656e46
dewangkoroputra <100069228+dewangkoroputra@users.noreply.github.com>
2022-02-27 16:05:44 +0700

    Add files via upload

---
+++ ComputeMethods5026211167.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+
+public class ComputeMethods5026211167 {
+    public double fToC (double F) {
+        return 5 / 9.0 * (F - 32);
+    }
+
+    public double hypotenuse (double a, double b) {
+        return Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+    }
+
+    public int roll () {
+
+        Random random = new Random();
+
+        int dice1 = random.nextInt(6) + 1;
+        int dice2 = random.nextInt(6) + 1;
+
+        return dice1 + dice2;
+    }
+    
+}
